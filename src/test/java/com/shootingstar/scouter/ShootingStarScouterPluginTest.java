58eb9e3a0104101c1ea4a17f7231b9cc789c4803
Deph0 <Deph0@users.noreply.github.com>
2025-11-23 14:10:45 +0100

    vscode build & debug

--- src/test/java/com/shootingstar/scouter/ShootingStarScouterPluginTest.java
+++ src/test/java/com/shootingstar/scouter/ShootingStarScouterPluginTest.java
@@ -5,6 +5,7 @@ import net.runelite.client.externalplugins.ExternalPluginManager;
 
 public class ShootingStarScouterPluginTest
 {
+	@SuppressWarnings("unchecked")
 	public static void main(String[] args) throws Exception
 	{
 		ExternalPluginManager.loadBuiltin(ShootingStarScouterPlugin.class);
