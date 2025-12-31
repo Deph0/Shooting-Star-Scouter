fe6cb12f51de988bda4f0a683bc52af3f1b38093
Deph0 <Deph0@users.noreply.github.com>
2025-12-31 16:44:06 +0100

    Move code to bottom instead

--- src/main/java/com/shootingstar/scouter/websocket/MessageBuilder.java
+++ src/main/java/com/shootingstar/scouter/websocket/MessageBuilder.java
@@ -10,23 +10,6 @@ import com.shootingstar.scouter.models.StarData;
 public class MessageBuilder
 {
     /**
-     * Build a star update message from a StarData object
-     * @param starData The StarData object
-     * @return The JSON string message
-     */
-    public static String buildStarUpdate(StarData starData)
-    {
-        return buildStarUpdate(
-            starData.getWorld(),
-            starData.getTier(),
-            starData.getLocation(),
-            starData.isBackup(),
-            starData.getFoundBy(),
-            starData.getFirstFound()
-        );
-    }
-
-    /**
      * Build a star update message
      * @param world The world number where the star was found
      * @param tier The tier of the star (1-9)
@@ -68,5 +51,18 @@ public class MessageBuilder
         
         message.add("data", data);
         return message.toString();
+    }
+
+    /**
+     * Build a star update message from a StarData object
+     * @param starData The StarData object
+     * @return The JSON string message
+     */
+    public static String buildStarUpdate(StarData starData)
+    {
+        return buildStarUpdate(
+            starData.getWorld(), starData.getTier(), starData.getLocation(),
+            starData.isBackup(), starData.getFoundBy(), starData.getFirstFound()
+        );
     }
 }
